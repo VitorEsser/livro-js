7e00679eff872ff4535ec21660f2ac7fc1ca6c3f
VitorEsser <vitoresser10@gmail.com>
2026-02-25 20:02:05 +0100

    feat: finished chapter 03

--- cap03/ex3_4.js
+++ cap03/ex3_4.js
@@ -2,3 +2,9 @@ const prompt = require('prompt-sync')()
 
 const peso = Number(prompt("Peso da Ração (kg): "))
 const consumo = Number(prompt("Consumo Diário (gr): "))
+
+const pesoEmGrama = peso * 1000
+const duracao = Math.floor(pesoEmGrama / consumo)
+const sobra = pesoEmGrama % consumo
+console.log(`Duração: ${duracao} dias`)
+console.log(`Sobra: ${sobra}gr`)
