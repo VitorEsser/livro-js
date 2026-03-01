e3e85bd07a493a7d2e105ff56bd1cef205aa2e7c
VitorEsser <vitoresser10@gmail.com>
2026-03-01 17:43:19 +0100

    feat: finished examples from chapter 4

---
+++ cap04/nodejs/ex4_8.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")()
+const numero = Number(prompt("Número (centena): "))
+
+if (numero < 100 || numero >= 1000) {
+  console.log("Erro...número dever ser uma centena")
+  return
+}
+
+const num1 = Math.floor(numero / 100)
+const sobra = numero % 100
+const num2 = Math.floor(sobra / 10)
+const num3 = sobra % 10
+
+console.log(`Número invertido ${num3}${num2}${num1}`)
